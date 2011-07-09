9322a13c3b0c6cc7484e8fcac6c93260dbee8dad
ChrisJan00 <cjan_ynzn@yahoo.es>
2011-07-09 17:11:03 +0200

    Character sprite

--- player.js
+++ player.js
@@ -1,27 +1,29 @@
-//var AnimationControl = function(indices) {
-//	var self = this;
-//	self.frameIndices = indices.slice();
-//	self.frameIndex = 0;
-//	self.updateFrame = function() {
-//		self.frameIndex = (self.frameIndex+1)%self.frameIndices.length;
-//	}
-//	self.currentFrame = function() {
-//		return self.frameIndices[self.frameIndex];
-//	}
-//}
+var AnimationControl = function(indices) {
+	var self = this;
+	self.frameIndices = indices.slice();
+	self.frameIndex = 0;
+	self.updateFrame = function() {
+		self.frameIndex = (self.frameIndex+1)%self.frameIndices.length;
+	}
+	self.currentFrame = function() {
+		return self.frameIndices[self.frameIndex];
+	}
+}
 
 var Player = function(x,y) {
 	var self = this;
 	
 	self.keys = new( function() { this.check = function( str ) { return false; } } );
-	//self.animationStrip = new Image();
-    //self.animationStrip.src = "graphics/Player1.png";
-    //self.standingIndices = new AnimationControl([0]);
-	//self.walkingIndices = new AnimationControl([1,2,3]);
-	//self.jumpingIndices = new AnimationControl([4,5,6]);
-	//self.dyingIndices = new AnimationControl([7,8]);
-	//self.animationControl = self.standingIndices;
-	//self.stripChoice = 0;
+	self.animationStrip = new Image();
+    self.animationStrip.src = "char_am_01.png";
+    self.standingIndices = new AnimationControl([1]);
+	self.walkingRightIndices = new AnimationControl([0,1,2,1]);
+	self.walkingLeftIndices = new AnimationControl([0,1,2,1]);
+	self.walkingUpIndices = new AnimationControl([0,1,2,1]);
+	self.walkingDownIndices = new AnimationControl([0,1,2,1]);
+	
+	self.animationControl = self.standingIndices;
+	self.stripChoice = 0;
 	
 	self.x = x;
 	self.y = y;
@@ -32,26 +34,20 @@ var Player = function(x,y) {
 	self.speed = 200;
 	self.drag = 0.04;
 	self.accel = 0.1;
-	self.w = 16;
-	self.h = 16;
-	self.frameCount = 9;
+	self.w = 13;
+	self.h = 28;
 	self.currentFrame = 0;
 	self.dts = 0;
-	//self.gravityBlock = false;
-	//self.gravityCap = 2;
-	//self.jumpStrength = 1.15;
-	//self.frameDelay = 100;
-	//self.frameTimer = self.frameDelay;
-	//self.lookingRight = true;
+	self.frameDelay = 150;
+	self.frameTimer = self.frameDelay;
 	
 	self.complete = function() {
-	//	if (!self.animationStrip.complete)
-	//		return false;
+		if (!self.animationStrip.complete)
+			return false;
 		return true;
 	}
 	
 	self.init = function () {
-	//	self.prepareAnimationAngles();
 	}
 	
 	self.update = function(dt) {
@@ -70,54 +66,31 @@ var Player = function(x,y) {
 		}
 		
 		// animation
-		//self.frameTimer -= dt;
-		//if (self.frameTimer<=0) {
-		//	self.updateFrame();
-		//	self.frameTimer = self.frameDelay;
-		//}
-		
-		//self.updateStripChoice();
+		self.frameTimer -= dt;
+		if (self.frameTimer<=0) {
+			self.updateFrame();
+			self.frameTimer = self.frameDelay;
+		}
 	}
 	
-	//self.updateFrame = function() {
-	//	if (self.standing()) {
-	//		if (((GLOBAL.gravityDir==0||GLOBAL.gravityDir==1) && self.vx!=0) || 
-	//			((GLOBAL.gravityDir==2||GLOBAL.gravityDir==3) && self.vy!=0)) {
-	//			self.animationControl = self.walkingIndices;
-	//		} else {
-	//			self.animationControl = self.standingIndices;
-	//		}
-	//	} else {
-	//		self.animationControl = self.jumpingIndices;
-	//	}
-	//	
-	//	self.animationControl.updateFrame();
-	//} 
-	
-	//self.updateStripChoice = function() {
-	//	switch(GLOBAL.gravityDir) {
-	//		case 0:
-	//			if (self.vx>0) self.lookingRight = true;
-	//			if (self.vx<0) self.lookingRight = false;
-	//			self.stripChoice = self.lookingRight?0:1; 
-	//		break;
-	//		case 1: 
-	//			if (self.vx>0) self.lookingRight = true;
-	//			if (self.vx<0) self.lookingRight = false;
-	//			self.stripChoice = self.lookingRight?2:3;
-	//		break;
-	//		case 2:
-	//			if (self.vy>0) self.lookingRight = true;
-	//			if (self.vy<0) self.lookingRight = false;
-	//			self.stripChoice = self.lookingRight?4:5;
-	//		break;
-	//		case 3:
-	//			if (self.vy>0) self.lookingRight = true;
-	//			if (self.vy<0) self.lookingRight = false;
-	//			self.stripChoice = self.lookingRight?6:7;
-	//		break;
-	//	}
-	//}
+	self.updateFrame = function() {
+		if (self.vx > 0) {
+			self.animationControl = self.walkingRightIndices;
+		} else
+		if (self.vx < 0) {
+			self.animationControl = self.walkingLeftIndices;
+		} else
+		if (self.vy < 0) {
+			self.animationControl = self.walkingUpIndices;
+		} else
+		if (self.vy > 0) {
+			self.animationControl = self.walkingDownIndices;
+		} else {
+			self.animationControl = self.standingIndices;
+		}
+		
+		self.animationControl.updateFrame();
+	} 
 	
 	self.updatedPos = function() {
 		var retVal = {}
@@ -125,28 +98,6 @@ var Player = function(x,y) {
 		retVal.y = Math.floor(self.y + self.vy * self.speed * self.dts + 0.5);
 		return retVal;
 	}
-	
-	//self.standing = function() {
-	//	switch(GLOBAL.gravityDir) {
-	//	case 0:
-	//		return GLOBAL.level.collided(self.x, self.y + self.speed*self.dts, self.w, self.h);
-	//	case 1:
-	//		return GLOBAL.level.collided(self.x, self.y - self.speed*self.dts, self.w, self.h);
-	//	case 2:
-	//		return GLOBAL.level.collided(self.x - self.speed*self.dts, self.y, self.w, self.h);
-	//	case 3:
-	//		return GLOBAL.level.collided(self.x + self.speed*self.dts, self.y, self.w, self.h);
-	//	}
-	//}
-	
-	//self.applyGravity = function() {
-	//	switch(GLOBAL.gravityDir) {
-	//		case 0: self.vy = Math.min(self.gravityCap, self.vy + self.dts * GLOBAL.gravity/self.speed); break;
-	//		case 1: self.vy = Math.max(-self.gravityCap, self.vy - self.dts * GLOBAL.gravity/self.speed); break;
-	//		case 2: self.vx = Math.max(-self.gravityCap, self.vx - self.dts * GLOBAL.gravity/self.speed); break;
-	//		case 3: self.vx = Math.min(self.gravityCap, self.vx + self.dts * GLOBAL.gravity/self.speed); break;
-	//	}
-	//}
 	
 	self.parseKeys = function() {
 		var mx = 0;
@@ -204,70 +155,14 @@ var Player = function(x,y) {
 			//GLOBAL.gameContext.drawImage(self.strips[self.stripChoice], 
 			//	self.animationControl.currentFrame()*self.w, 0, self.w, self.h, self.ix, self.iy, self.w, self.h);
 			var ctxt = graphics.peopleLayer.getContext("2d");
-			ctxt.fillStyle = "#00FF00";
-			ctxt.fillRect(self.ix, self.iy, self.w, self.h);
+			//ctxt.fillStyle = "#00FF00";
+			//ctxt.fillRect(self.ix, self.iy, self.w, self.h);
+			ctxt.drawImage(self.animationStrip,
+				self.animationControl.currentFrame()*self.w, 0, self.w, self.h, self.ix, self.iy, self.w, self.h);
 			graphics.mark(self.ix, self.iy, self.w, self.h);
 			graphics.redraw();
 		}
 	
 	}
-	
-//	self.prepareAnimationAngles = function() {
-//		self.strips = [];
-//		
-//		for (var i=0;i<8;i++)
-//			self.strips[i] = self.modifiedStrip(i);
-//	}
-	
-		
-//	self.modifiedStrip = function( stripIndex ) {
-//		var canvas = document.createElement('canvas');
-//		var ctx = canvas.getContext('2d');
-//		
-//		var fc = self.animationStrip.width / self.w;
-//		for (var i=0;i<fc;i++) {
-//			
-//			switch (stripIndex) {
-//			case 1: // dl
-//				ctx.translate((i*2+1)*self.w, 0);
-//				ctx.scale(-1, 1);
-//				break;
-//			case 2: // ur
-//				ctx.translate(0, self.h);
-//				ctx.scale(1, -1);
-//				break;
-//			case 3: // ul
-//				ctx.translate((i*2+1)*self.w, self.h);
-//				ctx.scale(-1, -1);
-//				break;
-//			case 4: // ld
-//				ctx.translate((i+1)*self.w, -self.h*i);
-//				ctx.rotate(GLOBAL.PI / 2);
-//				break;
-//			case 5: // lu
-//				ctx.translate((i+1)*self.w, self.h*(i+1));
-//				ctx.scale(1, -1);
-//				ctx.rotate(GLOBAL.PI / 2);
-//				break;
-//			case 6: // rd
-//				ctx.translate(i*self.w, -self.h*i);
-//				ctx.scale(1, -1);
-//				ctx.rotate(-GLOBAL.PI / 2);
-//				break;
-//			case 7: // ru
-//				ctx.translate(i*self.w, self.h*(i+1));
-//				ctx.rotate(-GLOBAL.PI / 2);
-//				break;
-//			
-//			}
-//			
-//			ctx.drawImage(self.animationStrip, i*self.w,0,self.w,self.h, i*self.w,0,self.w,self.h);
-//			
-//			// reset (we don't want to acumulate transformations)
-//			ctx.setTransform(1,0,0,1,0,0);
-//		}
-//		
-//		return canvas;
-//	}
 	
 }
